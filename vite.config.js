5bb9052639a547da85c341c50af0b6432eadfc42
Vitor Gomes <vitoralm0613@gmail.com>
2026-01-27 10:22:03 -0300

    PWA ativado

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         scope: '/',
-        start_url: '/',
+        start_url: '/dash',
         orientation: 'portrait',
         icons: [
           {
